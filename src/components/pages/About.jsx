aae7a4eba056dfd85cdd745b68a7eab9e24e832a
Jepoy Factoran <factoran20@gmail.com>
2022-06-01 22:56:50 +0800

    minor ui changes

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,17 +1,33 @@
 function About() {
   return (
     <div className="mockup-window border bg-base-300">
-      <div className="flex justify-center px-4 py-16 bg-base-200">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        nihil harum labore mollitia! Dolore velit debitis quam nobis mollitia,
-        vel sunt omnis et at enim. Dolor corrupti maiores ut ducimus!! Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Explicabo ipsa est
-        quod expedita unde, dignissimos voluptatem excepturi sint voluptates
-        reprehenderit enim molestiae veniam! Est quo ipsa sit vel rem ipsam!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-        voluptatem, fuga quidem vitae nesciunt dolore animi saepe? Nihil, eius
-        magni similique voluptatem tenetur saepe odio odit. Accusamus
-        consequatur iste eligendi!
+      <div className="flex justify-center pt-10 bg-base-200">
+        <h1 className="text-3xl font-bold">This is the About page</h1>
+      </div>
+      <div className="flex justify-center p-10 bg-base-200">
+        <h1 className="text-md">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+          placeat eum quibusdam deleniti dolorem, quos inventore magni.
+          Praesentium, eveniet? Harum asperiores voluptates, quibusdam iste
+          numquam quisquam et repellat possimus sed? Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Eveniet pariatur repellendus ipsa
+          consectetur, explicabo deserunt nobis quis aliquam libero! Et
+          blanditiis est optio iste eaque, corrupti tenetur nulla! Sunt, magni?
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+          quisquam doloremque! Voluptate inventore, maiores, soluta totam
+          reiciendis qui eveniet aliquam quibusdam cupiditate consequatur odit
+          magnam sint sequi autem illum dolores! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Corporis fuga accusamus atque sapiente
+          doloremque illo! Non id, quisquam consequatur obcaecati placeat
+          perferendis dolorem nisi quam amet assumenda fugiat, repellendus modi?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas cum
+          vero aliquid vel omnis neque nesciunt deserunt, ducimus recusandae
+          nihil dolore dolores accusamus similique reiciendis excepturi laborum,
+          deleniti ad. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quis velit aliquam deserunt! Cupiditate quia officiis omnis in
+          consectetur repellat ipsa tempora voluptas eos expedita dolor, maxime
+          veritatis nisi ipsum autem.
+        </h1>
       </div>
     </div>
   );
--- src/components/pages/Page404.jsx
+++ src/components/pages/Page404.jsx
@@ -1,5 +1,36 @@
 function Page404() {
-  return <div>notfound</div>;
+  return (
+    <div className="mockup-window border bg-base-300">
+      <div className="flex justify-center pt-10 bg-base-200">
+        <h1 className="text-3xl font-bold">Aw, Snap!</h1>
+      </div>
+      <div className="flex justify-center p-10 bg-base-200">
+        <h1 className="text-md">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+          placeat eum quibusdam deleniti dolorem, quos inventore magni.
+          Praesentium, eveniet? Harum asperiores voluptates, quibusdam iste
+          numquam quisquam et repellat possimus sed? Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Eveniet pariatur repellendus ipsa
+          consectetur, explicabo deserunt nobis quis aliquam libero! Et
+          blanditiis est optio iste eaque, corrupti tenetur nulla! Sunt, magni?
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+          quisquam doloremque! Voluptate inventore, maiores, soluta totam
+          reiciendis qui eveniet aliquam quibusdam cupiditate consequatur odit
+          magnam sint sequi autem illum dolores! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Corporis fuga accusamus atque sapiente
+          doloremque illo! Non id, quisquam consequatur obcaecati placeat
+          perferendis dolorem nisi quam amet assumenda fugiat, repellendus modi?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In quas cum
+          vero aliquid vel omnis neque nesciunt deserunt, ducimus recusandae
+          nihil dolore dolores accusamus similique reiciendis excepturi laborum,
+          deleniti ad. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quis velit aliquam deserunt! Cupiditate quia officiis omnis in
+          consectetur repellat ipsa tempora voluptas eos expedita dolor, maxime
+          veritatis nisi ipsum autem.
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default Page404;
