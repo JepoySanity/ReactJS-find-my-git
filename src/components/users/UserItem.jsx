170645fbdae2b14100b501459fa21fab5d879868
Jepoy Factoran <factoran20@gmail.com>
2022-06-02 09:17:03 +0800

    finalize user item ui

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,17 +1,23 @@
+import { Link } from "react-router-dom";
 function UserItem({ user: { login, avatar_url } }) {
   return (
-    <>
-      <div class="alert alert-warning shadow-lg">
+    <div className="card shadow-md compact side bg-warning">
+      <div className="flex-row items-center space-x-4 card-body">
         <div>
           <div className="avatar">
-            <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-              <img src={avatar_url} />
+            <div className="rounded-full ring ring-primary shadow w-14 h-14">
+              <img src={avatar_url} alt="Profile" />
             </div>
           </div>
-          <h1 className="text-xl font-bold p-4">{login}</h1>
+        </div>
+        <div>
+          <h1 className="card-title text-black font-bold text-xl">{login}</h1>
+          <Link to={`users/${login}`} className="text-primary underline">
+            View Profile
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
