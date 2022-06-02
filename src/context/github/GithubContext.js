5f1e20ada801a492acb1df7f419f360a64585aa4
Jepoy Factoran <factoran20@gmail.com>
2022-06-02 23:08:44 +0800

    use reducer instead of using usestate

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,22 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import GitHubReducer from "./GitHubReducer";
 
 const GitHubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 export const GitHubProvider = ({ children }) => {
-  const [users, setusers] = useState([]);
-  const [isLoading, setisLoading] = useState(true);
+  const initialState = {
+    users: [],
+    isLoading: true,
+  };
+
+  const [state, dispatch] = useReducer(GitHubReducer, initialState);
 
   const getUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`);
     const data = await response.json();
-    setusers(data);
-    setisLoading(false);
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
   };
 
   return (
-    <GitHubContext.Provider value={{ users, isLoading, getUsers }}>
+    <GitHubContext.Provider
+      value={{ users: state.users, isLoading: state.isLoading, getUsers }}
+    >
       {children}
     </GitHubContext.Provider>
   );
