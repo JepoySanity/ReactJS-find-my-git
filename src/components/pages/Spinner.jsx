ba7b423393c747a2792cd18103226542d62a364b
Jepoy Factoran <factoran20@gmail.com>
2022-06-04 16:22:25 +0800

    resize spinner

--- src/components/pages/Spinner.jsx
+++ src/components/pages/Spinner.jsx
@@ -2,7 +2,7 @@ import loader from "../../assets/loader/loader.gif";
 function Spinner() {
   return (
     <div className="w-100 mt-auto">
-      <img width={180} className="text-center mx-auto" src={loader} alt="" />
+      <img width={100} className="text-center mx-auto" src={loader} alt="" />
     </div>
   );
 }
